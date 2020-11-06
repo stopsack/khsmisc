070a3b5c4c0f79335dac0210d31d4cc5ffe776d9
stopsack <stopsack@mskcc.org>
2020-11-06 08:17:22 -0500

    add: tabulate_rowcol() instead of abtab(); tidy selections; rates() parameters "risk_time" and "by"

--- R/tables.R
+++ R/tables.R
@@ -1,6 +1,81 @@
 # Functions for (descriptive) tables
 
-#' Contingency Table
+#' Contigency Table: Tabulate Rows Times Columns
+#'
+#' @import tidyverse purrr dplyr forcats
+#' @importFrom rlang `:=`
+# @import dplyr forcats tidyr purrr
+#'
+#' @description Tabulates \code{row} variable times \code{col} variable.
+#'   Supports tidy evaluation for \code{row} and \code{col}; see examples.
+#'
+#' @param data Data frame (tibble).
+#' @param row Row variable.
+#' @param col Column variable.
+#' @param maxlevels Optional. Maximum number of levels for rows and columns.
+#'   If a row or column variable has more than this number of levels,
+#'   for example because it is actually a continuous variable,
+#'   then only a warning, not a hard error, will be shown. Defaults to 30.
+#' @param rowsums Optional. Show row sums? Defaults to TRUE.
+#' @param colsums Optional. Show column sums? Defaults to TRUE.
+#'
+#' @return Tibble.
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' mtcars %>%
+#'   tabulate_rowcol(gear, carb)
+#'
+#' # Swap rows and columns, turn off totals:
+#' mtcars %>%
+#'   tabulate_rowcol(row = carb, col = gear,
+#'                   rowsum = FALSE, colsum = FALSE)
+tabulate_rowcol <- function(data,
+                            row,
+                            col,
+                            maxlevels = 30,
+                            rowsums   = TRUE,
+                            colsums   = TRUE) {
+  topname <- paste(colnames(data %>% dplyr::select({{ row }}))[1], "X",
+                   colnames(data %>% dplyr::select({{ col }}))[1])
+  data <- data %>%
+    dplyr::rename(rowvar = {{ row }}, colvar = {{ col }})
+  var1lvls <- length(levels(factor(data$rowvar)))
+  var2lvls <- length(levels(factor(data$colvar)))
+  if(var2lvls > maxlevels)
+    stop(paste("No table is printed because the column variable has", var2lvls,
+               "levels. Maximum is set to", maxlevels))
+  if(var1lvls > maxlevels)
+    stop(paste("No table is printed because the row variable has", var1lvls,
+               "levels. Maximum is set to", maxlevels))
+
+  result <- data %>%
+    dplyr::mutate_at(.vars = dplyr::vars(.data$rowvar, .data$colvar),
+                     .funs = as.factor) %>%
+    dplyr::mutate_at(.vars = dplyr::vars(.data$rowvar, .data$colvar),
+                     .funs = forcats::fct_explicit_na) %>%
+    dplyr::group_by_at(.vars = dplyr::vars(.data$rowvar, .data$colvar)) %>%
+    dplyr::summarize(n = dplyr::n()) %>%
+    tidyr::pivot_wider(names_from = .data$colvar, values_from = .data$n) %>%
+    dplyr::ungroup() %>%
+    dplyr::rename({{ topname }} := 1) %>%
+    dplyr::mutate_at(.vars = dplyr::vars(-{{ topname }}), .funs = ~ifelse(is.na(.), 0, .))
+  if(rowsums == TRUE) {
+    result <- result %>%
+      dplyr::mutate_at(.vars = dplyr::vars({{ topname }}), .funs = as.character)
+    result <- result %>%
+      dplyr::bind_rows(dplyr::summarize_all(result, .funs = function(x) {
+        if(is.numeric(x)) sum(x) else "Total" }))
+  }
+  if(colsums == TRUE)
+    result <- result %>%
+    dplyr::mutate(Total = purrr::pmap_dbl(.l = dplyr::select(result, -{{ topname }}),
+                                          .f = sum))
+  result
+}
+
+#' Contingency Table (Deprecated, Use tabulate_rowcol Instead)
 #'
 #' @import tidyverse purrr dplyr forcats
 #' @importFrom rlang `:=`
@@ -9,6 +84,9 @@
 #' @description Tabulates X times Y. Using a data frame (tibble)
 #'   as input, the first and second columns of the data will be used
 #'   as rows and columns, respectively.
+#'
+#'   This function has been deprecated. Use
+#'   \code{\link[khsmisc]{tabulate_rowcol}} instead.
 #'
 #' @param data Data frame (tibble).
 #' @param maxlevels Optional. Maximum number of levels for rows and columns.
@@ -70,7 +148,6 @@ mytab <- function(data,
   }
 }
 
-
 #' Summary of All Numeric Variables in a Data Frame
 #'
 #' @import tidyr dplyr
@@ -80,6 +157,9 @@ mytab <- function(data,
 #'   can be stratified by one or more categorical variable(s).
 #'
 #' @param data Data frame (tibble).
+#' @param ... Optional. Variables to summarize. If not provided,
+#'   all numeric variables will be summarized. Supports tidy evaluation;
+#'   see examples.
 #' @param by Optional. Categorical variable(s) to stratify results by.
 #' @param na.rm Optional. Drop missing values from summary statatistics?
 #'   If set to \code{FALSE}, summary statistics may be missing in the presence of
@@ -103,41 +183,49 @@ mytab <- function(data,
 #' @examples
 #' data(mtcars)
 #' mtcars %>%
-#'   select(mpg, cyl, hp, am, gear, carb) %>%
 #'   tsummary()
 #'
-#' # Remove certain summary statistics:
+#' # Select specific variables and
+#' # remove some summary statistics:
 #' mtcars %>%
-#'   select(mpg, cyl, hp, am, gear, carb) %>%
-#'   tsummary() %>%
+#'   tsummary(mpg, cyl, hp, am, gear, carb) %>%
 #'   select(-mean, -sd, -sum)
 #'
 #' # Stratify by 'gear':
 #' mtcars %>%
-#'   select(mpg, hp, gear, carb) %>%
-#'   tsummary(by = "gear")
+#'   tsummary(mpg, hp, carb, by = gear)
 #'
 #' # Stratify by 'gear' and 'am':
 #' mtcars %>%
-#'   select(mpg, hp, gear, am, carb) %>%
-#'   tsummary(by = c("am", "gear"))
+#'   tsummary(mpg, hp, carb, by = c(am, gear))
 tsummary <- function(data,
+                     ...,
                      by    = NULL,
                      na.rm = TRUE) {
   data <- data %>% labelled::remove_labels()
+  by <- data %>% dplyr::select({{ by }}) %>% names()
+  newdata <- data %>% select(!!!rlang::enquos(...), all_of(by))
+  if(ncol(newdata) == ncol(data %>% select(all_of(by))))
+    newdata <- data
+  data <- newdata
+
   if(is.null(by)) {
     data <- data %>%
       dplyr::select_if(is.numeric) %>%
-      tidyr::gather(key = "variable", value = "value") %>%
+      tidyr::pivot_longer(cols = everything(),
+                          names_to = "variable",
+                          values_to = "value") %>%
       dplyr::group_by(.data$variable)
   } else {
-    var1 <- "variable"
-    to_keep <- c(data %>% dplyr::select_if(is.numeric) %>% names(), by)
+    to_keep <- c(data %>% dplyr::select_if(is.numeric) %>% names(),
+                 dplyr::all_of(by))
     data <- data %>%
-      dplyr::select(dplyr::one_of(to_keep)) %>%
-      tidyr::gather(key = "variable", value = "value",
-                    -dplyr::contains(paste(by))) %>%
-      dplyr::group_by_at(dplyr::vars(dplyr::all_of(var1), dplyr::all_of(by)))
+      dplyr::select(dplyr::all_of(to_keep)) %>%
+      tidyr::pivot_longer(cols = c(-dplyr::all_of(by)),
+                          names_to = "variable",
+                          values_to = "value") %>%
+      dplyr::group_by_at(dplyr::vars(dplyr::one_of("variable"),
+                                     dplyr::all_of(by)))
   }
   data %>% dplyr::summarize(rows   = dplyr::n(),
                             obs    = sum(!is.na(.data$value)),
@@ -205,6 +293,8 @@ mygt <- function(df) {
 #'   * Rounding of continuous values to one decimal digit by default.
 #'
 #' @param data Data frame/tibble to print. Required.
+#' @param ... Optional: Rows to include in table. If none are provided,
+#'   all variables will be shown. Supports tidy evaluation; see examples.
 #' @param by Optional: Stratification variable for columns.
 #' @param overall Optional: Show "Overall" column if stratifying? Defaults to \code{TRUE}.
 #' @param label Optional: Label for strata. If empty, the variable label or
@@ -217,7 +307,6 @@ mygt <- function(df) {
 #' @param statistic Optional: Summary statistic to the shown for specific variables, e.g.:
 #'   \code{list(a_specific_variable ~ "{min}, {max}")}.
 #'   Passed on to \code{\link[gtsummary]{tbl_summary}(statistic = ...)}.
-#' @param ... Optional: Passed on to \code{\link[gtsummary]{tbl_summary}}.
 #'
 #' @return Formatted table. Continuous variables are
 #'   median (quartile 1, quartile 3); categorical variables are
@@ -226,15 +315,30 @@ mygt <- function(df) {
 #'
 #' @examples
 #' data(mtcars)
+#'
+#' # Stratify by "gear":
 #' mtcars %>%
-#'   table1(by = "gear")
+#'   table1(by = gear)
+#'
+#' # Show only selected variables in rows:
+#' mtcars %>%
+#'   table1(cyl, mpg, disp, by = gear)
 table1 <- function(data,
+                   ...,
                    by        = NULL,
                    overall   = TRUE,
                    label     = "",
                    digits    = list(all_continuous() ~ c(1, 1)),
-                   statistic = NULL,
-                   ...) {
+                   statistic = NULL) {
+  by <- data %>% dplyr::select({{ by }}) %>% names()
+  if(length(by) > 1)
+    stop(paste0("Only one stratification (column) variable is supported. Input was: by = '",
+         paste(by, sep = " ", collapse = " "), "'."))
+  newdata <- data %>% select(!!!rlang::enquos(...), all_of(by))
+  if(ncol(newdata) == ncol(data %>% select(all_of(by))))
+    newdata <- data
+  data <- newdata
+
   options(gtsummary.tbl_summary.percent_fun = function(x) sprintf("%.0f", 100 * x))
   mystats <- list(N ~ "{n}")
   if(!is.null(statistic))
@@ -243,7 +347,7 @@ table1 <- function(data,
     data %>%
       dplyr::mutate(N = 1) %>%
       dplyr::select(.data$N, dplyr::everything()) %>%
-      gtsummary::tbl_summary(statistic = mystats, digits = digits, ...) %>%
+      gtsummary::tbl_summary(statistic = mystats, digits = digits) %>%
       gtsummary::bold_labels() %>%
       gtsummary::as_gt(include = -tab_footnote) %>%
       mytabstyle()
@@ -260,7 +364,7 @@ table1 <- function(data,
         dplyr::rename(by = dplyr::one_of(by)) %>%
         dplyr::filter(!is.na(by)) %>%
         labelled::copy_labels_from(from = data) %>%
-        gtsummary::tbl_summary(by = by, statistic = mystats, digits = digits, ...) %>%
+        gtsummary::tbl_summary(by = by, statistic = mystats, digits = digits) %>%
         gtsummary::add_overall() %>%
         gtsummary::modify_header(stat_0 = "**Overall**", stat_by = "**{level}**") %>%
         gtsummary::bold_labels()
@@ -269,7 +373,7 @@ table1 <- function(data,
         dplyr::mutate(N = 1) %>%
         dplyr::select(.data$N, dplyr::everything()) %>%
         dplyr::rename(by = dplyr::one_of(by)) %>%
-        gtsummary::tbl_summary(by = by, statistic = mystats, digits = digits, ...) %>%
+        gtsummary::tbl_summary(by = by, statistic = mystats, digits = digits) %>%
         gtsummary::modify_header(stat_by = "**{level}**") %>%
         gtsummary::bold_labels()
     }
@@ -299,22 +403,31 @@ table1 <- function(data,
 #'   then stratified results will be returned.
 #' @param time Variable indicating follow-up time.
 #' @param event Variable indicating events (event = 1, censor = 0).
-#' @param conversion Divisor for follow-up time. Defaults to 1.
+#' @param by Optional. Variable to stratify results by. Defaults
+#'   to how the dataset is grouped, i.e., if it is grouped already,
+#'   results will be shown by the stratification variable.
+#' @param risk_time Optional. Time point at which cumulative incidence
+#'   (risk) should be calculated. Provide in original units of follow-up
+#'   time, i.e., before potential division by the \code{conversion} factor.
+#'   If not provided, defaults to the end of follow-up, which may not
+#'   be the best interpretable timepoint.
+#' @param conversion Optional. Divisor for follow-up time. Defaults to 1.
 #'   Set \code{conversion = 12} if \code{time} is in months and
 #'   rates per person-years are desired.
-#' @param factor Person-time units for the denominator. Defaults to \code{1000},
+#' @param factor Optional. Person-time units for the denominator. Defaults to \code{1000},
 #'   e.g., for rates per 1000 person-years of follow-up.
-#' @param rate_digits Decimal digits to display for rates. Defaults to 1.
-#' @param risk_digits Decimal digits to display for
+#' @param rate_digits Optional. Decimal digits to display for rates. Defaults to 1.
+#' @param risk_digits Optional. Decimal digits to display for
 #'   cumulative incidence (risk). Defaults to 1.
 #'
 #' @return Tibble:
-#'   *  \code{n} Count of participants
-#'   *  \code{Events} Count of events
-#'   *  \code{Person-time} Sum of person-time
-#'   *  \code{Rate per person-time} Incidence rate
-#'   *  \code{Risk (Kaplan-Meier, %)} Cumulative incidence
-#'      at the end of follow-up, with log-log 95% confidence intervals
+#'   *  \code{n} Count of participants.
+#'   *  \code{Events} Count of events.
+#'   *  \code{Person-time} Sum of person-time.
+#'   *  \code{Rate per person-time} Incidence rate.
+#'   *  \code{Risk (%)} Cumulative incidence.
+#'      at time \code{risk_time} or, if not provided, at the end of follow-up,
+#'      with log-log 95% confidence intervals.
 #'
 #' @export
 #'
@@ -329,28 +442,35 @@ table1 <- function(data,
 #'
 #' # Unstratified
 #' ovarian %>%
-#'   rates(time = "futime", event = "fustat")
+#'   rates(time = futime, event = fustat)
 #'
-#' # Convert time to person-years
+#' # Show risk (cumulative incidence) at 25 years,
+#' # show follow-up time to person-years
 #' ovarian %>%
-#'   rates(time = "futime", event = "fustat", conversion = 12)
+#'   rates(time = futime, event = fustat,
+#'         risk_time = 25 * 12, conversion = 12)
 #'
-#' # Stratified by "ecog.ps":
+#' # Stratified by "ecog.ps" and "resid.ds":
 #' ovarian %>%
-#'   group_by(ecog.ps) %>%
-#'   rates(time = "futime", event = "fustat", conversion = 12)
-rates <- function(data, time, event, conversion = 1,
+#'   rates(time = futime, event = fustat,
+#'         by = c(ecog.ps, resid.ds),
+#'         risk_time = 25 * 12, conversion = 12)
+rates <- function(data, time, event, by, risk_time = NULL, conversion = 1,
                   rate_digits = 1, risk_digits = 1, factor = 1000) {
   # The helper function is needed because survfit() does not retain grouping of data
-  rates_helper <- function(data, time, event, conversion, rate_digits, risk_digits, factor) {
-    fit <- survival::survfit(formula = stats::as.formula(paste0("Surv(", time, ", ", event, ") ~ 1")),
+  rates_helper <- function(data, conversion, risk_time, rate_digits, risk_digits, factor) {
+    fit <- survival::survfit(formula = Surv(time = timevar, event = eventvar) ~ 1,
                              data    = data, type = "kaplan-meier", conf.type = "log-log")
-    cuminc <- 1-c(utils::tail(summary(fit)$surv,  n = 1),
-                  utils::tail(summary(fit)$upper, n = 1),
-                  utils::tail(summary(fit)$lower, n = 1))
+    if(is.null(risk_time))
+      cuminc <- 1 - c(utils::tail(summary(fit)$surv,  n = 1),
+                      utils::tail(summary(fit)$upper, n = 1),
+                      utils::tail(summary(fit)$lower, n = 1))
+    else
+      cuminc <- 1 - c(summary(fit, time = risk_time)$surv,
+                      summary(fit, time = risk_time)$upper,
+                      summary(fit, time = risk_time)$lower)
+
     data %>%
-      dplyr::rename(eventvar = dplyr::one_of(event),
-                    timevar  = dplyr::one_of(time)) %>%
       dplyr::summarize(
         `n`           = dplyr::n(),
         `Events`      = sum(.data$eventvar),
@@ -368,7 +488,7 @@ rates <- function(data, time, event, conversion = 1,
           "-",
           format(round(.data$ul * factor, digits = rate_digits), nsmall = rate_digits),
           ")"),
-        `Risk (Kaplan-Meier, %)`    = paste0(
+        `Risk (%)`    = paste0(
           format(round(cuminc[1] * 100, digits = risk_digits),
                  nsmall = risk_digits),
           " (",
@@ -381,6 +501,13 @@ rates <- function(data, time, event, conversion = 1,
       dplyr::select(-.data$ir, -.data$ll, -.data$ul)
   }
 
+  by <- data %>% dplyr::select({{ by }}) %>% names()
+  if(length(by) > 0)
+    data <- data %>% dplyr::group_by_at(dplyr::all_of(by))
+
+  data <- data %>%
+    dplyr::rename(eventvar = {{ event }},
+                  timevar  = {{ time }})
   if(dplyr::is_grouped_df(data))
     data <- data %>% tidyr::nest()
   else
@@ -389,8 +516,7 @@ rates <- function(data, time, event, conversion = 1,
     dplyr::summarize(results = purrr::map(
       .x = data,
       .f = ~rates_helper(data        = .x,
-                         time        = time,
-                         event       = event,
+                         risk_time   = risk_time,
                          conversion  = conversion,
                          rate_digits = rate_digits,
                          risk_digits = risk_digits,
